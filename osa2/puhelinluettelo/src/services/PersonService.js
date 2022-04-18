66d439303d925d01ccf97425070c7565e9963a37
Frans Welin <73482123+franswel@users.noreply.github.com>
2022-04-18 21:36:37 +0300

    Fix conflict

--- osa2/puhelinluettelo/src/services/PersonService.js
+++ osa2/puhelinluettelo/src/services/PersonService.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-const baseUrl = '/api/persons'
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-
-const annul = (id) => {
-    axios.delete(`${baseUrl}/${id}`)
-}
-
-=======
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
@@ -44,5 +20,4 @@ const annul = (id) => {
     axios.delete(`${baseUrl}/${id}`)
 }
 
->>>>>>> 4f9cb5e9e5c39972d2d1fd265e7790aed9e1e16c
 export default { getAll, create, update, annul }
